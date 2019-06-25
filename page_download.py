576e1e78c6b7f72ab0331e46e5f750f95ab8d4f6
monkey-tree <mr_hjq@163.com>
2019-06-25 18:14:31 +0800

    Timer is added.

--- page_download.py
+++ page_download.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import time
 import chardet
 import urllib.request
 from urllib.error import URLError, HTTPError, ContentTooShortError
@@ -10,18 +11,21 @@ def download(url, user_agent = 'wswp', num_retries = 2, charset = 'utf-8'):
     request.add_header('User-agent', user_agent)
     try:
         print("Download starts.")
+        time_start = time.time()
         resp = urllib.request.urlopen(request)
-        print("Downloads completes.")
+        print("Downloads completes and eclips {}s.".format(time.time() - time_start))
+        time_start = time.time()
         cs = resp.headers.get_content_charset()
-        print("Charset gotten.")
+        print("Charset gotten and eclips {}s".format(time.time() - time_start))
         html_raw = resp.read()
         print("html read is done.")
         if not cs:
             print("Charset detection start.")
+            time_start = time.time()
             csdet = chardet.detect(html_raw)
             print("Charset detection completes.")
             cs = csdet['encoding']
-            print("Charset: {}".format(cs))
+            print("Charset: {} and eclips {}s.".format(cs, time.time() - time_start))
 
         html = html_raw.decode(cs)
         resp.close
@@ -41,6 +45,7 @@ def crawl_sitemap(url):
     # extract the sitemap links
     print("Sitemap links extraction starts.")
     links = re.findall ('<loc>(.*?)</loc>', sitemap)
+    print("links extracted: \n {}".format(links))
     print("Sitemap links extraction ends.")
     for link in links:
         html = download(link)
--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="D:\projects\web_scraper\venv"
+$env:VIRTUAL_ENV="C:\projects\web_scraper\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
