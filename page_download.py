f5982badbcbae98a4b33818ebfc73f773ebdaa0d
monkey-tree <mr_hjq@163.com>
2019-06-25 18:19:52 +0800

    Revert "Timer is added."
    This reverts commit 576e1e78c6b7f72ab0331e46e5f750f95ab8d4f6.

--- page_download.py
+++ page_download.py
@@ -1,6 +1,5 @@
 import sys
 import re
-import time
 import chardet
 import urllib.request
 from urllib.error import URLError, HTTPError, ContentTooShortError
@@ -11,21 +10,18 @@ def download(url, user_agent = 'wswp', num_retries = 2, charset = 'utf-8'):
     request.add_header('User-agent', user_agent)
     try:
         print("Download starts.")
-        time_start = time.time()
         resp = urllib.request.urlopen(request)
-        print("Downloads completes and eclips {}s.".format(time.time() - time_start))
-        time_start = time.time()
+        print("Downloads completes.")
         cs = resp.headers.get_content_charset()
-        print("Charset gotten and eclips {}s".format(time.time() - time_start))
+        print("Charset gotten.")
         html_raw = resp.read()
         print("html read is done.")
         if not cs:
             print("Charset detection start.")
-            time_start = time.time()
             csdet = chardet.detect(html_raw)
             print("Charset detection completes.")
             cs = csdet['encoding']
-            print("Charset: {} and eclips {}s.".format(cs, time.time() - time_start))
+            print("Charset: {}".format(cs))
 
         html = html_raw.decode(cs)
         resp.close
@@ -45,7 +41,6 @@ def crawl_sitemap(url):
     # extract the sitemap links
     print("Sitemap links extraction starts.")
     links = re.findall ('<loc>(.*?)</loc>', sitemap)
-    print("links extracted: \n {}".format(links))
     print("Sitemap links extraction ends.")
     for link in links:
         html = download(link)
